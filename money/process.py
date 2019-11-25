e019b6fcf75dfadad7292010b203544cbc8b320e
nathan <natebanion@gmail.com>
2019-11-25 08:41:16 -0500

    Add functions to prepare transactions for analysis.

--- money/process.py
+++ money/process.py
@@ -11,6 +11,8 @@ Notes:
     - Processing configurations.
 
 """
+import pandas as pd
+from . import category as cg
 
 
 def process(credit_paths, checking_paths, budget_path):
@@ -30,34 +32,102 @@ def process(credit_paths, checking_paths, budget_path):
     pass
 
 
-def prep_credit(df, categories, edits=None):
+def prep_credit(df, categories, edits=None, source=None):
     """Prepare credit card transaction data for processing.
+
+    The input dataset includes columns "Transaction Date" and "Post Date". The
+    first of these columns is the date that a transaction occurred. The second
+    is the date that a transaction was processed and posted to the transaction
+    history. This function uses the transaction date.
 
     Arguments:
         df : Pandas dataframe with transaction data.
         categories (dict): Regex patterns for each category.
         edits (dict): Index-specific manual categorizations.
+        source (str): Data source, to be included as a variable.
 
     Returns:
-        A Pandas dataframe of prepped credit card data.
+        A Pandas dataframe of prepped data.
 
     """
-    pass
+    cols = {
+        "date": "Transaction Date",
+        "desc": "Description",
+        "amount": "Amount"
+    }
+    return prep_transactions(df, cols, categories, edits=edits, source=source)
 
 
-def prep_checking(df, categories, edits=None):
+def prep_checking(df, categories, edits=None, source=None):
     """Prepare checking transaction data for processing.
 
     Arguments:
         df : Pandas dataframe with transaction data.
         categories (dict): Regex patterns for each category.
         edits (dict): Index-specific manual categorizations.
+        source (str): Data source, to be included as a variable.
 
     Returns:
-        A Pandas dataframe of prepped checking data.
+        A Pandas dataframe of prepped data.
 
     """
-    pass
+    cols = {
+        "date": "Posting Date",
+        "desc": "Description",
+        "amount": "Amount"
+    }
+    return prep_transactions(df, cols, categories, edits=edits, source=source)
+
+
+def prep_transactions(df, cols, categories, edits=None, source=None):
+    """Prepare transaction data for processing.
+
+    This function performs the following transformations:
+
+    - Subset columns to transaction dates, descriptions, and amounts.
+    - Rename columns with shorter, standardized names.
+    - Reverse the index to start with the earliest transaction.
+    - Update variable ``date`` to have the datetime dtype.
+    - Add variable ``source`` to document the transaction source.
+    - Add variable ``category`` to categorize the transaction.
+
+    Argument ``cols`` is a dict with the following keys:
+
+    - ``date``: The date of the transaction.
+    - ``desc``: The transaction description.
+    - ``amount``: The transaction dollar amount.
+
+    The values for this dict are the names of the columns that have these values
+    in the input dataframe.
+
+    The function reverses the index so that transactions keep the same indices
+    as the input ``df`` grows. The transaction source data are cumulative
+    datasets, and new cases appear at the top (index 0), changing the indices
+    for all transactions in the data. Reversing the index makes sure that all
+    transactions have constant indicies that correspond to transaction order.
+
+    Arguments:
+        df : Pandas dataframe with transaction data.
+        cols (dict): Columns to process from the source ``df``.
+        categories (dict): Regex patterns for each category.
+        edits (dict): Index-specific manual categorizations.
+        source (str): Data source, to be included as a variable.
+
+    Returns:
+        A Pandas dataframe of prepped transaction data.
+
+    """
+    colnames = ["date", "desc", "amount"]
+    keeps = [cols[c] for c in colnames]
+    renames = {cols[c]: c for c in colnames}
+    return (df.loc[:, keeps]
+              .rename(columns=renames)
+              .set_index(df.index[::-1])  # Reverse index.
+              .assign(source=source)
+              .assign(date=lambda x: pd.to_datetime(x["date"]))
+              .assign(category=lambda x: cg.categorize(x["desc"],
+                                                       categories,
+                                                       edits=edits)))
 
 
 def categories(budget):
--- money/tests/test_process.py
+++ money/tests/test_process.py
@@ -1,5 +1,53 @@
 import pytest
+import pandas as pd
+import pandas.testing as pdt
 from .. import process
+
+
+@pytest.fixture
+def expected_prep_result():
+    """Expected dataframe after preparing credit or checking data."""
+    data = {
+        "date": pd.Series(("01/01/2019", "01/02/2019"), dtype="datetime64[ns]"),
+        "desc": ("item0", "item1"),
+        "amount": (-10, -10),
+        "source": ("test", "test"),
+        "category": ("cat0", "cat1")
+    }
+    return pd.DataFrame(data)[::-1]
+
+
+def test_prep_credit(expected_prep_result):
+    categories = {"cat0": ["item0"]}
+    edits = {1: "cat1"}
+    data_df = {
+        "Transaction Date": ("01/02/2019", "01/01/2019"),
+        "Post Date": ("01/03/2019", "01/02/2019"),
+        "Description": ("item1", "item0"),
+        "Category": ("NA", "NA"),
+        "Type": ("sale", "sale"),
+        "Amount": (-10, -10)
+    }
+    df = pd.DataFrame(data_df)
+    result = process.prep_credit(df, categories, edits=edits, source="test")
+    pdt.assert_frame_equal(result, expected_prep_result)
+
+
+def test_prep_checking(expected_prep_result):
+    categories = {"cat0": ["item0"]}
+    edits = {1: "cat1"}
+    data_df = {
+        "Details": ("DEBIT", "DEBIT"),
+        "Posting Date": ("01/02/2019", "01/01/2019"),
+        "Description": ("item1", "item0"),
+        "Amount": (-10, -10),
+        "Type": ("ACH_DEBIT", "ACH_DEBIT"),
+        "Balance": (100, 110),
+        "Check or Slip #": (None, None)
+    }
+    df = pd.DataFrame(data_df)
+    result = process.prep_checking(df, categories, edits=edits, source="test")
+    pdt.assert_frame_equal(result, expected_prep_result)
 
 
 def test_categories():
